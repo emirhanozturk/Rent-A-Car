3d230ab6b5bd727a4e0577d573768b62a6726ce4
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2021-03-23 11:40:11 +0300

    .

--- Entities/DTOs/RentalDto.cs
+++ Entities/DTOs/RentalDto.cs
@@ -9,13 +9,7 @@ namespace Entities.DTOs
     {
         public int Id { get; set; }
         public int CarId { get; set; }
-        public int CustomerId { get; set; }
         public string BrandName { get; set; }
-        public string ColorName { get; set; }
-        public int ModelYear { get; set; }
-        public decimal DailyPrice { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public DateTime RentDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public string CompanyName { get; set; }
